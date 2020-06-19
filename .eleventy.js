8f751e9f3d3f41936ce763143bab856d8f9ac252
Evgeny Orekhov <eorekhov@axcient.com>
2020-06-19 21:42:21 +0300

    Add CNAME
    Ref #124 Use js.org

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
-module.exports = {
-  dir: {
-    input: "site",
-  },
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("CNAME");
+
+  return {
+    dir: {
+      input: "site",
+    },
+  };
 };
