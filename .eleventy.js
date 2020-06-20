1244ce5fc37b1c91a75e67e55e04166ff1335eb1
Evgeny Orekhov <eorekhov@axcient.com>
2020-06-21 00:09:28 +0300

    Fix #128 Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
 
   return {
     dir: {
