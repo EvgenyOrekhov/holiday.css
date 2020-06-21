0ac652f1877ee108f947deae991945a8b12375d9
Evgeny Orekhov <eorekhov@axcient.com>
2020-06-21 13:21:46 +0300

    Fix #130 Kitchen Sink - external resources not working

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("favicon-16x16.png");
   eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("nyan-cat.mp4");
+  eleventyConfig.addPassthroughCopy("sample.pdf");
+  eleventyConfig.addPassthroughCopy("t-rex-roar.mp3");
 
   return {
     dir: {
