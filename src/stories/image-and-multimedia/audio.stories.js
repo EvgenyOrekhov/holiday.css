a24101bab846d1bea3879036011f1c1937c22efa
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2021-06-20 15:50:13 +0300

    Add "Image and multimedia"

---
+++ src/stories/image-and-multimedia/audio.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default {
+  title: "Image and multimedia/<audio>",
+};
+
+export const audio = () => (
+  <audio controls src="https://holidaycss.js.org/t-rex-roar.mp3"></audio>
+);
+
+audio.storyName = "<audio>";
---
+++ src/stories/image-and-multimedia/map-area.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default {
+  title: "Image and multimedia/<map>, <area>",
+};
+
+export const mapArea = () => (
+  <>
+    <map name="infographic">
+      <area
+        shape="poly"
+        coords="130,147,200,107,254,219,130,228"
+        href="https://developer.mozilla.org/docs/Web/HTML"
+        alt="HTML"
+      />
+      <area
+        shape="poly"
+        coords="130,147,130,228,6,219,59,107"
+        href="https://developer.mozilla.org/docs/Web/CSS"
+        alt="CSS"
+      />
+      <area
+        shape="poly"
+        coords="130,147,200,107,130,4,59,107"
+        href="https://developer.mozilla.org/docs/Web/JavaScript"
+        alt="JavaScript"
+      />
+    </map>
+    <img
+      usemap="#infographic"
+      width="260"
+      height="232"
+      src="https://interactive-examples.mdn.mozilla.net/media/examples/mdn-info2.png"
+      alt="MDN infographic"
+    />
+  </>
+);
+
+mapArea.storyName = "<map>, <area>";
