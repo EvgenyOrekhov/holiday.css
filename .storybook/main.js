8165e7bdbcf2752946e4bb34194fb7c93597caa1
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2026-05-08 19:03:51 +0300

    Fix Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,7 @@
-
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
-  "framework": "@storybook/react-vite"
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
+  framework: "@storybook/react-vite",
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,22 @@
+import "./fix.css";
+
+import "modern-normalize";
+import "../src/css/mobile.css";
+import "../src/css/cursors.css";
+import "../src/css/nav.css";
+import "../src/css/themes/bulma.css";
+
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
+  tags: ["autodocs"],
 };
 
 export default preview;
