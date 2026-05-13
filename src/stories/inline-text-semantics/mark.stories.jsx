f8b5318f6186fee26960bead4867419a919d4f74
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2026-05-13 22:34:22 +0300

    Fix #316 `<code>` inside `<mark>` is not highlighted
    And the same with `<kbd>`.

--- src/stories/inline-text-semantics/mark.stories.jsx
+++ src/stories/inline-text-semantics/mark.stories.jsx
@@ -4,6 +4,11 @@ export default {
   title: "Inline text semantics/<mark>",
 };
 
-export const mark = () => <mark>Example</mark>;
+export const mark = () => (
+  <mark>
+    The <code>&lt;mark&gt;</code> element highlights text.
+    <kbd>&lt;kbd&gt;</kbd> test.
+  </mark>
+);
 
 mark.storyName = "<mark>";
