6fb21f5e5f3ed654902586f04e0c18355bbb5aa1
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2026-05-14 21:56:56 +0300

    Fix #317 Extra padding at the top of `<fieldset>`

--- src/stories/forms/fieldset-legend.stories.jsx
+++ src/stories/forms/fieldset-legend.stories.jsx
@@ -47,3 +47,57 @@ fieldsetWithLegendDisabled.storyName = "<fieldset> with <legend> disabled";
 fieldsetWithLegendDisabled.args = {
   disabled: true,
 };
+
+export const FieldsetWithRadios = (args) => (
+  <fieldset {...args}>
+    <input type="radio" id="choice-1" name="choice" value="1" />{" "}
+    <label for="choice-1">Choice 1</label>
+    <br />
+    <input type="radio" id="choice-2" name="choice" value="2" />{" "}
+    <label for="choice-2">Choice 2</label>
+    <br />
+    <input type="radio" id="choice-3" name="choice" value="3" />{" "}
+    <label for="choice-3">Choice 3</label>
+  </fieldset>
+);
+
+export const FieldsetWithRadiosAndLegend = (args) => (
+  <fieldset {...args}>
+    <legend>Radio</legend>
+    <input type="radio" id="choice-1" name="choice" value="1" />{" "}
+    <label for="choice-1">Choice 1</label>
+    <br />
+    <input type="radio" id="choice-2" name="choice" value="2" />{" "}
+    <label for="choice-2">Choice 2</label>
+    <br />
+    <input type="radio" id="choice-3" name="choice" value="3" />{" "}
+    <label for="choice-3">Choice 3</label>
+  </fieldset>
+);
+
+export const FieldsetWithCheckboxes = (args) => (
+  <fieldset {...args}>
+    <input type="checkbox" id="check-1" name="check-1" value="check1" />{" "}
+    <label for="check-1">Check 1</label>
+    <br />
+    <input type="checkbox" id="check-2" name="check-2" value="check2" />{" "}
+    <label for="check-2">Check 2</label>
+    <br />
+    <input type="checkbox" id="check-3" name="check-3" value="check3" />{" "}
+    <label for="check-3">Check 3</label>
+  </fieldset>
+);
+
+export const FieldsetWithCheckboxesAndLegend = (args) => (
+  <fieldset {...args}>
+    <legend>Checkbox</legend>
+    <input type="checkbox" id="check-1" name="check-1" value="check1" />{" "}
+    <label for="check-1">Check 1</label>
+    <br />
+    <input type="checkbox" id="check-2" name="check-2" value="check2" />{" "}
+    <label for="check-2">Check 2</label>
+    <br />
+    <input type="checkbox" id="check-3" name="check-3" value="check3" />{" "}
+    <label for="check-3">Check 3</label>
+  </fieldset>
+);
