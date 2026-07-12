c3d0c2ef6df82ca776ad3bd6859038b91320f3a6
Joey@macstudio <johuang@aligntech.com>
2026-07-12 09:41:42 +0800

    docs: add hgroup example

---
+++ src/stories/content-sectioning/hgroup.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default {
+  title: "Content sectioning/<hgroup>",
+};
+
+export const hgroup = {
+  render: () => (
+    <hgroup>
+      <h1>Example</h1>
+      <p>Supporting text</p>
+    </hgroup>
+  ),
+  name: "<hgroup>",
+};
